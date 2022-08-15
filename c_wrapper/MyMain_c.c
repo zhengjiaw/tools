f04cdfa8a4b99f29035314872d296ed3ae6221a7
linxi <linxi177229@gmail.com>
2022-08-15 21:36:09 +0800

    c_wrapper

---
+++ c_wrapper/MyMain_c.c
@@ -0,0 +1,9 @@
+#include "MyWrapper.h"
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+        struct MyClass* c = newMyClass();
+        MyClass_int_set(c, 3);
+        printf("%i\n", MyClass_int_get(c));
+        deleteMyClass(c);
+}
